12c15c4bdb6419d0d25b93e3078953096d6b8339
Zheng Weihua <9607271+ZhengWeihuaYNU@users.noreply.github.com>
2025-05-07 01:41:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,29 +20,31 @@ from layers import GraphTransformerNet,  DataAug
 from loss import AGCLoss, contrastive_loss
 from utils import knngraph, sim, cluster_acc
 
-def evaluate():
+def evaluate(data_name= 'pbmc4k', real_label= True, net_params= {'num_layers': 1, 'in_dim': expr.shape[1], 'hidden_dim': 32, 
+                                                                 'out_dim': 32, 'final_embed': 16, 'num_heads': 4, 'dropout': 0.5, 
+                                                                 'attn_drop': 0.5, 'add_drop': 0.0, 'lap_pos_enc': True, 
+                                                                 'pos_enc_dim':eig_num, 'cluster': 8, 'num_neighbors': 400,
+                                                                 'batch_size': 512, 'tau': 0.5, 'cls_thres': 0.5}):
     #if gpu is availbale then use gpu
     device= 'cuda' if torch.cuda.is_available() else 'cpu'
-    data_h5 = h5py.File('pbmc4k.h5') #data file, containing X as the cell*gene count matrix and Y as the real label
-    expr = np.array(data_h5.get('X'))
-    real_label = np.array(data_h5.get('Y')).reshape(-1)
+    expr= pd.read_csv(f'{data_name}.txt', sep= '\t', index_col= 0) 
+    if real_label== True:
+        real_label= pd.read_csv('real_label.txt', sep= '\t', index_col= 0)
+        adata.obs['Ground Truth'] = real_label
+        real_label = torch.tensor(LabelEncoder().fit_transform(adata.obs['Ground Truth']))
     adata = preprocess(expr) #data preprocessing includes quality control, normalization, log-transformation and HVGs selection
     expr = torch.tensor(adata.X[:, adata.var['highly_variable']].astype(np.float32))
-    adata.obs['Ground Truth'] = real_label
     pca_embed = torch.tensor(adata.obsm['X_pca'].copy())
-    real_label = torch.tensor(LabelEncoder().fit_transform(adata.obs['Ground Truth']))
-    num_neighbors= 400
+    
+    num_neighbors= net_params['num_neighbors']
     eig_num= 20
     gradient_clipping = 10
     num_epochs = 500
-    batch_size= 512
-    tau = 0.5
+    batch_size= net_params['batch_size']
+    tau = net_params['tau']
     lam = 0.5
-    cls_thres = 0.5
-    agc = AGCLoss(device= device)
-    net_params = {'num_layers': 1, 'in_dim': expr.shape[1], 'hidden_dim': 32, 'out_dim': 32, 'final_embed': 16,
-                  'num_heads': 4, 'dropout': 0.5, 'attn_drop': 0.5, 'add_drop': 0.0, 'lap_pos_enc': True, 
-                  'pos_enc_dim':eig_num, 'cluster': len(np.unique(real_label.cpu()))}    
+    cls_thres = net_params['cls_thres']
+    agc = AGCLoss(device= device)  
     adj, g_edges= knngraph(pca_embed, num_neighbors)
     lap_sym = get_laplacian(g_edges, normalization='sym')
     aaa= torch.zeros((adj.shape))
@@ -76,10 +78,8 @@ def evaluate():
             batch = batch.to(device)
             input1, input2 = aug_model(batch.x), aug_model(batch.x)
             output1, output2, contrast1, contrast2, cluster1, cluster2 = model(input1, input2, batch.edge_index, batch.laplacian)
-            # 注意模型输出的行数不是batch_size，而是为了计算batch_size个节点的表示所需要的节点个数，这些节点的id是batch.n_id，其中前batch_size个
-            # id就是最终得到的batch_size个节点的id，即batch.input_id
             final_cluster = ((cluster1 + cluster2) / 2)[:len(batch.input_id),
-                            :].detach()  # 形状为(batch_size,net_params['clusters'])
+                            :].detach()  
             sub_adj_eye = adj_eye[batch.input_id, :][:, batch.input_id]
             sim_cls = sim(final_cluster, final_cluster) - torch.eye(sub_adj_eye.shape[0]).to(device)
             loss_instance = contrastive_loss(contrast1[:len(batch.input_id), :], contrast2[:len(batch.input_id), :],
@@ -92,19 +92,24 @@ def evaluate():
             optimizer.step()
         model.eval()
         with torch.no_grad():
-            output1, output2, contrast1, contrast2, cluster1, cluster2 = model(expr, expr, g_edges, lp_matrix)
-            final_cluster = ((cluster1 + cluster2) / 2)
-            sim_cls = sim(final_cluster, final_cluster) - (torch.eye(adj_eye.shape[0])).to(device)
-            loss_instance_train = contrastive_loss(contrast1, contrast2, tau,
-                                                   adj_eye, sim_cls, cls_thres)
-            loss_cluster_train = agc(cluster1, cluster2, adj_eye)
-            loss_train = lam * loss_instance_train + (1 - lam) * loss_cluster_train
-            ari = adjusted_rand_score(real_label.cpu(), torch.argmax(final_cluster, 1).cpu())
+            cluster_list= []
+            for batch in test_loader:
+                batch= batch.to(device)
+                input1, input2= batch.x, batch.x
+                output1, output2, contrast1, contrast2, cluster1, cluster2 = model(input1, input2, batch.edge_index, batch.laplacian)
+                pred_cluster = ((cluster1 + cluster2) / 2)[:len(batch.input_id),
+                                :].detach()  
+                cluster_list.append(pred_cluster)
+            final_cluster= torch.cat(cluster_list) 
+            #ari = adjusted_rand_score(real_label.cpu(), torch.argmax(final_cluster, 1).cpu())
+            loss_train= np.mean(loss_list)
+            loss_instance_train= np.mean(ins_loss_list)
+            loss_cluster_train= np.mean(cls_loss_list)
         if loss_train < min_train_loss:
             counter = 0
             min_train_loss = loss_train
             best_t = epoch
-            torch.save(model.state_dict(), 'best_model.pkl')
+            torch.save(model, f'best_model_{data_name}.pth')
         else:
             counter += 1
         if counter >= early_stop_counter:
@@ -114,7 +119,7 @@ def evaluate():
             print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss_train:.4f}')
             print(f'Epoch {epoch + 1}/{num_epochs}, Instance Loss: {loss_instance_train:.4f}')
             print(f'Epoch {epoch + 1}/{num_epochs}, Cluster Loss: {loss_cluster_train:.4f}')
-            print(f'Ari: {ari}')
+            #print(f'Ari: {ari}')
             print(f'current best epoch: {best_t + 1}')
     print('Loading {}th epoch'.format(best_t))
     model.eval()
@@ -123,14 +128,17 @@ def evaluate():
     final_output = (output1 + output2) / 2
     final_output = final_output.cpu().detach().numpy()
     final_cluster = ((cluster1 + cluster2) / 2).argmax(1).cpu()
-    ari = adjusted_rand_score(real_label, final_cluster)
-    nmi = normalized_mutual_info_score(real_label, final_cluster)
-    ca = cluster_acc(real_label, final_cluster)
     end_time= time.time()
     print(f'time cost: {end_time- start_time}')
-    print(f'evaluated ari: {ari}')
-    print(f'evaluated nmi: {nmi}')
-    print(f'evaluated ca: {ca}')
+    if real_label==True:
+        ari = adjusted_rand_score(real_label, final_cluster)
+        nmi = normalized_mutual_info_score(real_label, final_cluster)
+        ca = cluster_acc(real_label, final_cluster)
+        print(f'evaluated ari: {ari}')
+        print(f'evaluated nmi: {nmi}')
+        print(f'evaluated ca: {ca}')
+    print('Done')
+
 if __name__ == '__main__':
     evaluate()
 
